3cfe132db318dedcb0163c0c3e1d18ec9993d63c
Aleksandr Savichev <aleksandrs.hd@yandex.ru>
2023-02-01 21:37:28 +0300

    Создана страница с фильмами

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 
 import Header from "../Header/Header";
-import Profile from "../Profile/Profile";
+import Films from "../Films/Films";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Profile />
+      <Films />
     </div>
   );
 }
