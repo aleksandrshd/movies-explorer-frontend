0ddb0ad16231a193c4a02db6f44b7c47af0e4f9f
Aleksandr Savichev <aleksandrs.hd@yandex.ru>
2023-02-22 15:26:26 +0300

    Cделаны AboutMe, Footer в 320px

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,5 @@
+import {Route, Routes} from 'react-router-dom';
+
 import './App.css';
 
 import Header from "../Header/Header";
@@ -9,17 +11,32 @@ import NavTab from "../NavTab/NavTab";
 import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
 import AboutMe from "../AboutMe/AboutMe";
+import Register from "../Register/Register";
+import Login from "../Login/Login";
+import PageNotFound from "../PageNotFound/PageNotFound";
 
 function App() {
   return (
     <div className="App">
-      <Header loggedIn={false} />
-      <Promo />
-      <NavTab />
-      <AboutProject />
-      <Techs />
-      <AboutMe />
-      {/*<Footer />*/}
+      <Routes>
+        <Route path="/sign-in" element={<Login />}/>
+        <Route path="/sign-up">
+          <Header loggedIn={false} />
+          <Register />
+        </Route>
+        <Route exact path="/">
+          <Header loggedIn={false} />
+          <Promo />
+          <NavTab />
+          <AboutProject />
+          <Techs />
+          <AboutMe />
+          <Footer />
+        </Route>
+        <Route path="*">
+          <PageNotFound />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,8 +7,8 @@ export default function Footer() {
 
       <h2 className="footer__header">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
       <div className="footer__container">
-        <p className="footer__copyright">&copy; 2023</p>
-        <div>
+        <p className="footer__copyright">&copy;2023</p>
+        <div className="footer__links">
           <a className="footer__link" href="https://practicum.yandex.ru/">Яндекс.Практикум</a>
           <a className="footer__link" href="https://github.com/">Github</a>
         </div>
