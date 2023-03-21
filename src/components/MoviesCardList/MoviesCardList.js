09b6d146df3d78cca470da02046b98235e00219c
Aleksandr Savichev <aleksandrs.hd@yandex.ru>
2023-03-21 16:25:49 +0300

    fix / добавлен key в MoviesCard

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -7,7 +7,7 @@ export default function MoviesCardList({moviesArray, savedFilms}) {
       <section className="films">
         <ul className="films__list">
           {moviesArray.map(item => (
-            <MoviesCard id={item.id} data={item} savedFilms={savedFilms}/>
+            <MoviesCard id={item.id} key={item.id} data={item} savedFilms={savedFilms}/>
           ))}
         </ul>
         {!savedFilms && <button className="films__button-more">Ещё</button>}
