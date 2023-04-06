59912a74c29ade3ec67bd763016c2f206d22eb5b
Aleksandr <aleksandrs.hd@yandex.ru>
2023-04-06 16:19:16 +0300

    feature / реализован переход на ютюб-трейлер фильма кликом по карточке

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,10 +1,9 @@
-import {convertDuration, setIsLiked} from "../../utils/utils";
+import { convertDuration, setIsLiked } from "../../utils/utils";
 
 import './MoviesCard.css';
 
-export default function MoviesCard({card, savedFilms, onMovieLike, favouriteMovies}) {
+export default function MoviesCard({ card, savedFilms, onMovieLike, favouriteMovies }) {
 
-  /*const isLiked = false;*/
   const { isLiked } = setIsLiked(card, favouriteMovies);
 
   const handleMovieLike = () => {
@@ -13,18 +12,20 @@ export default function MoviesCard({card, savedFilms, onMovieLike, favouriteMovi
 
   return (
     <li className="movie">
-      <img className="movie__image"
-           src={ savedFilms ? `${card.image}` : `https://api.nomoreparties.co/${card.image.url}` }
-           alt="Постер к фильму"/>
-      <div className="movie__container">
-        <h2 className="movie__title">{card.nameRU}</h2>
-        <button
-          className={`movie__like-button ${isLiked ? 'movie__like-button_liked' : ''} ${savedFilms ? 'movie__like-button_saved' : ''}`}
-          type="button"
-          onClick={handleMovieLike}/>
-      </div>
-      <p
-        className="movie__duration">{convertDuration(card.duration).hours > 0 && `${convertDuration(card.duration).hours}ч `}{`${convertDuration(card.duration).minutes}м`}</p>
+      <a className="movie__link" href={card.trailerLink} target="_blank">
+        <img className="movie__image"
+             src={savedFilms ? `${card.image}` : `https://api.nomoreparties.co/${card.image.url}`}
+             alt="Постер к фильму"/>
+        <div className="movie__container">
+          <h2 className="movie__title">{card.nameRU}</h2>
+          <button
+            className={`movie__like-button ${isLiked ? 'movie__like-button_liked' : ''} ${savedFilms ? 'movie__like-button_saved' : ''}`}
+            type="button"
+            onClick={handleMovieLike}/>
+        </div>
+        <p
+          className="movie__duration">{convertDuration(card.duration).hours > 0 && `${convertDuration(card.duration).hours}ч `}{`${convertDuration(card.duration).minutes}м`}</p>
+      </a>
     </li>
   );
 }
