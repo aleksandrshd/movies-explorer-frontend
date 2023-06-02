46becfc927eb2e720973386bd0bbb75788a82615
Aleksandr <aleksandrs.hd@yandex.ru>
2023-06-02 16:05:30 +0300

    fix / доработано отображение информационных сообщений SearchForm для мобильных разрешений

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,67 +1,71 @@
-import { useCallback, useEffect, useState } from "react";
+import {useCallback, useEffect, useState} from "react";
 
 import './SearchForm.css';
 
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 import useSearchForm from "../../hooks/useSearchForm";
-import { debounce } from "../../utils/utils";
+import {debounce} from "../../utils/utils";
 
-export default function SearchForm({ filterOn, setFilterOn, keyWord, setKeyWord, nothingFound }) {
+export default function SearchForm({filterOn, setFilterOn, keyWord, setKeyWord, nothingFound}) {
 
-  const [checkboxUnderInput, setCheckboxUnderInput] = useState(false);
+    const [checkboxUnderInput, setCheckboxUnderInput] = useState(false);
 
-  // Установка значения положения чекбокса короткометражек в зависимости от ширины окна просмотра при первоначальной отрисовке страницы
-  useEffect(() => {
+    // Установка значения положения чекбокса короткометражек в зависимости от ширины окна просмотра при первоначальной отрисовке страницы
+    useEffect(() => {
 
-    window.innerWidth < 640 ? setCheckboxUnderInput(true) : setCheckboxUnderInput(false);
+        window.innerWidth < 640 ? setCheckboxUnderInput(true) : setCheckboxUnderInput(false);
 
-  }, [])
+    }, [])
 
-  // Установка значения положения чекбокса короткометражек в зависимости от ширины окна просмотра при изменении ширины окна просмотра
-  useEffect(() => {
+    // Установка значения положения чекбокса короткометражек в зависимости от ширины окна просмотра при изменении ширины окна просмотра
+    useEffect(() => {
 
-    const sizeListener = debounce(640, setCheckboxUnderInput, false, true);
+        const sizeListener = debounce(640, setCheckboxUnderInput, false, true);
 
-    window.addEventListener('resize', sizeListener);
+        window.addEventListener('resize', sizeListener);
 
-    return () => window.removeEventListener('resize', sizeListener);
+        return () => window.removeEventListener('resize', sizeListener);
 
-  }, [checkboxUnderInput]);
+    }, [checkboxUnderInput]);
 
-  const { value, setValue, searchEmpty, handleChange, handleSubmit } = useSearchForm(setKeyWord);
+    const {value, setValue, searchEmpty, handleChange, handleSubmit} = useSearchForm(setKeyWord);
 
-  useEffect(() => {
+    useEffect(() => {
 
-    /*if (keyWord) setValue(keyWord);*/
-    setValue(keyWord);
+        /*if (keyWord) setValue(keyWord);*/
+        setValue(keyWord);
 
-  }, [keyWord, setValue]);
+    }, [keyWord, setValue]);
 
-  const toggleFilter = useCallback((e) => setFilterOn(e.target.checked), [setFilterOn]);
+    const toggleFilter = useCallback((e) => setFilterOn(e.target.checked), [setFilterOn]);
 
-  return (
-    <div className="search">
-      <form className="search__form" onSubmit={handleSubmit}>
-        <div className="search__container search__container_input">
-          <div className="search__logo"/>
-          <input
-            className="search__input"
-            name="film"
-            type="text"
-            value={value}
-            onChange={handleChange}
-            placeholder="Фильм"/>
+    return (
+        <div className="search">
+            <form className="search__form" onSubmit={handleSubmit}>
+                <div className="search__container search__container_input">
+                    <div className="search__logo"/>
+                    <input
+                        className="search__input"
+                        name="film"
+                        type="text"
+                        value={value}
+                        onChange={handleChange}
+                        placeholder="Фильм"/>
+                </div>
+                <div className="search__container">
+                    <button className="search__button">Найти</button>
+                    <div className="search__border"/>
+                    {!checkboxUnderInput && <FilterCheckbox value={filterOn} onChange={toggleFilter}/>}
+                </div>
+            </form>
+            {!checkboxUnderInput && <span className="search__error">{searchEmpty && 'Введите ключевое слово'}</span>}
+            {!checkboxUnderInput && <span className="search__error">{nothingFound && 'Ничего не найдено'}</span>}
+            {checkboxUnderInput && <>
+                <FilterCheckbox value={filterOn} onChange={toggleFilter}/>
+                <span className="search__error">{searchEmpty && 'Введите ключевое слово'}</span>
+                <span className="search__error">{nothingFound && 'Ничего не найдено'}</span>
+            </>}
         </div>
-        <div className="search__container">
-          <button className="search__button">Найти</button>
-          <div className="search__border"/>
-          {!checkboxUnderInput && <FilterCheckbox value={filterOn} onChange={toggleFilter}/>}
-        </div>
-      </form>
-      <span className="search__error">{searchEmpty && 'Введите ключевое слово'}</span>
-      <span className="search__error">{nothingFound && 'Ничего не найдено'}</span>
-      {checkboxUnderInput && <FilterCheckbox value={filterOn} onChange={toggleFilter}/>}
-    </div>
-  );
+    );
 
 }
